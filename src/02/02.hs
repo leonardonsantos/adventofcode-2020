b451fab18f44406e497ea36df135354e2bfc594f
Leonardo Santos <leonardo.santos@klarna.com>
2020-12-03 13:29:55 +0100

    02: part 2

--- src/02/02.hs
+++ src/02/02.hs
@@ -6,7 +6,9 @@ main = do
   contents <- getContents
   let parsedLines = fmap parse (lines contents)
   let result = foldl (\acc (a,b,c,s) -> if (verify a b c s) then acc+1 else acc) 0 parsedLines
-  putStr $ show $ result
+  print $ show $ result
+  let result2 = foldl (\acc (a,b,c,s) -> if (verify2 a b c s) then acc+1 else acc) 0 parsedLines
+  print $ show $ result2
 
 parse :: String -> (Int, Int, Char, String)
 parse line = let
@@ -18,3 +20,16 @@ verify :: Int -> Int -> Char -> String -> Bool
 verify a b c s = let
   count = foldl (\acc x -> if x==c then acc+1 else acc) 0 s
   in (count >= a) && (count <= b)
+
+xor :: Bool -> Bool -> Bool
+xor a b = (a || b) && not (a && b)
+
+nth :: [a] -> Int -> Maybe a
+nth [] _ = Nothing
+nth xs i = if i < (length xs) then Just (xs !! i) else Nothing
+
+verify2 :: Int -> Int -> Char -> String -> Bool
+verify2 a b c s = let
+  condA = maybe False (== c) (nth s (a-1))
+  condB = maybe False (== c) (nth s (b-1))
+  in xor condA condB
