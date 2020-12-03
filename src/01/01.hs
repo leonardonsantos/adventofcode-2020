d9a71113bc5261f34ed86ed048042a47fb7286df
Leonardo Santos <leonardo.santos@klarna.com>
2020-12-03 11:06:18 +0100

    01: part 2

--- src/01/01.hs
+++ src/01/01.hs
@@ -3,9 +3,11 @@
 main = do
   contents <- getContents
   let pairs = pairSum (numbers contents) 2020
-  let result = fmap (\(a,b) -> a*b) pairs
-  -- putStr $ show $ result
-  mapM_ print result
+  let resultPair = fmap (\(a,b) -> a*b) pairs
+  putStr $ show $ resultPair
+  let triples = tripleSum (numbers contents) 2020
+  let resultTriples = fmap (\(a,b,c) -> a*b*c) triples
+  putStr $ show $ resultTriples
 
 numbers :: String -> [Int]
 numbers input =
@@ -19,3 +21,7 @@ pairSum [] _ = []
 pairSum (x:[]) _ = []
 pairSum xs sum = take 1 [(x,y) | x<-xs, let y = sum - x, elem y xs]
 
+tripleSum :: (Num a, Eq a) => [a] -> a -> [(a,a,a)]
+tripleSum [] _ = []
+tripleSum (x:[]) _ = []
+tripleSum xs sum = take 1 [(x,y,z) | x<-xs, y<-xs, let z = sum - x - y, elem z xs]
