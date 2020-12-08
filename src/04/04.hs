6e7adf5d0d42feadf9edc48b844473b1c4d3a51b
Leonardo Santos <leonardo.santos@klarna.com>
2020-12-08 16:58:44 +0100

    04: part 2

--- src/04/04.hs
+++ src/04/04.hs
@@ -1,6 +1,7 @@
 -- https://adventofcode.com/2020/day/4
 
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 main = do
   contents <- getContents
@@ -9,6 +10,9 @@ main = do
   let validatedRecords = fmap validate parsedRecords
   let countValidated = foldl (\acc x -> if x then acc+1 else acc) 0 validatedRecords
   print countValidated
+  let validatedRecords2 = fmap validate2 parsedRecords
+  let countValidated2 = foldl (\acc x -> if x then acc+1 else acc) 0 validatedRecords2
+  print countValidated2
 
 breakOnEmptyLine :: String -> [String]
 breakOnEmptyLine "" = []
@@ -47,3 +51,63 @@ validate parsedRecord = let
   myDict = Map.fromList parsedRecord
   presentFields = fmap (\x -> Map.member x myDict) ["byr","iyr","eyr","hgt","hcl","ecl","pid"]
   in foldl (&&) True presentFields
+
+allFromListChar :: String -> String -> Bool
+allFromListChar c s = let
+  digits = Set.fromList c
+  in foldl (&&) True $ fmap (\x -> Set.member x digits) s
+
+allDigits :: String -> Bool
+allDigits = allFromListChar "0123456789"
+
+validateNumber :: String -> String -> String -> Bool
+validateNumber s a b =
+  (allDigits s)
+  && (s >= a)
+  && (s <= b)
+
+validateYear :: String -> String -> String -> Bool
+validateYear s a b =
+  (length s <=4)
+  && (validateNumber s a b)
+
+validateHeight :: String -> Bool
+validateHeight s =
+  let
+    heightUnit = drop (length s - 2) s
+    numberHeight = take (length s - 2) s
+  in
+    if heightUnit == "cm"
+      then validateNumber numberHeight "150" "193"
+      else
+        if heightUnit == "in"
+          then validateNumber numberHeight "59" "76"
+          else False
+
+validateHcl :: String -> Bool
+validateHcl s =
+  let
+    x:xs = s
+  in
+    (x == '#')
+    && (allFromListChar "0123456789abcdef" xs)
+
+validateEcl :: String -> Bool
+validateEcl s = elem s ["amb","blu","brn","gry","grn","hzl","oth"]
+
+validatePid :: String -> Bool
+validatePid s =
+  (length s == 9)
+  && (allDigits s)
+
+validate2 parsedRecord = let
+  myDict = Map.fromList parsedRecord
+  presentFields = fmap (\x -> Map.member x myDict) ["byr","iyr","eyr","hgt","hcl","ecl","pid"]
+  in foldl (&&) True presentFields
+    && (validateYear (myDict Map.! "byr") "1920" "2002")
+    && (validateYear (myDict Map.! "iyr") "2010" "2020")
+    && (validateYear (myDict Map.! "eyr") "2020" "2030")
+    && (validateHeight (myDict Map.! "hgt"))
+    && (validateHcl (myDict Map.! "hcl"))
+    && (validateEcl (myDict Map.! "ecl"))
+    && (validatePid (myDict Map.! "pid"))
