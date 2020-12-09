0119d172279af3455734cc226b1bb9972ca811e4
Leonardo Santos <leonardo.santos@klarna.com>
2020-12-09 17:45:50 +0100

    05: part 2

--- src/05/05.hs
+++ src/05/05.hs
@@ -1,18 +1,22 @@
 -- https://adventofcode.com/2020/day/5
 
+import Data.List
+
 main = do
   contents <- getContents
-  let passports = lines contents
-  let parsedPassports = fmap parse passports
-  let maxP = maxPassport parsedPassports
-  print maxP
+  let tickets = lines contents
+  let parsedTickets = fmap parse tickets
+  let maxT = maxTicket parsedTickets
+  print maxT
+  let missingT = missingTicket parsedTickets
+  print missingT
 
 parse :: String -> (Int, Int, Int)
-parse passport =
+parse ticket =
   let
-    rows = take 7 passport
+    rows = take 7 ticket
     row = calculate 0 127 rows
-    columns = drop 7 passport
+    columns = drop 7 ticket
     column = calculate 0 7 columns
     passId = row * 8 + column
   in (row, column, passId)
@@ -30,7 +34,20 @@ parse passport =
               then calculate a auxMiddle xs
               else calculate (auxMiddle+1) b xs
 
-maxPassport :: [(Int,Int,Int)] -> Int
-maxPassport passports =
-  let passIds = fmap (\(_,_,i) -> i) passports
+maxTicket :: [(Int,Int,Int)] -> Int
+maxTicket tickets =
+  let passIds = fmap (\(_,_,i) -> i) tickets
   in foldl1 max passIds
+
+missingTicket :: [(Int,Int,Int)] -> Int
+missingTicket tickets =
+  let
+    passIds = sort $ fmap (\(_,_,i) -> i) tickets
+    (_, missingT) = foldl process (0,0) passIds
+  in
+    missingT
+  where
+    process (i,r) x =
+      if (x==i+1)
+        then (x,r)
+        else (x,i+1)
